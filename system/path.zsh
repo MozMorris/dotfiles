a56af62b53bb9f298ee21c1070fbaeced7c47761
MozMorris <moz@earthview.co.uk>
2013-05-04 08:57:18 -0400

    Update path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="~/bin/:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:$PATH"
+export PATH="$HOME/bin/:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
