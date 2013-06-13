ae86dcd7a1efcb0750e1f6f8fe8917b11405a210
Zach Holman <zach@zachholman.com>
2013-06-12 19:12:09 -0700

    Allow $EDITOR to be set up elsewhere
    Conflicts:
    	system/env.zsh

--- system/env.zsh
+++ system/env.zsh
@@ -2,7 +2,7 @@
 export ARCHFLAGS="-arch x86_64"
 
 # Only set this if we haven't set $EDITOR up somewhere else previously.
-if [[ "$EDITOR" == "" ]] ; then
+if [ "$EDITOR" == "" ] ; then
   # Use sublime for my editor.
   export EDITOR='subl'
 fi
